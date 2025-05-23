0b51da2320e7aed134708fc0b9c130c5c991b50c
AdityaZulkarnaen <adityazulkarnaen083@gmail.com>
2025-05-23 13:19:32 +0700

    feat: add DeltaTime to make game tick works the same in every OS

--- src/core/Entry.java
+++ src/core/Entry.java
@@ -2,6 +2,19 @@ package core;
 
 public class Entry {
     public static void main(String[] args) {
-        Window window = new Window("SCHMUPP");
+        Window window = new Window("Space War");
+
+        boolean runGame = true;
+
+        FPS.calcBeginTime();
+        while (runGame) {
+            //update the game state
+
+            //render the game
+            System.out.printf("%f\n", FPS.getDeltaTime());
+            //recalculate delta time
+            FPS.calcDeltaTime();
+        }
+
     }
 }
---
+++ src/core/FPS.java
@@ -0,0 +1,26 @@
+package core;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public class FPS {
+    private FPS() {};
+
+    private static Duration fpsDeltatime = Duration.ZERO;
+    private static Duration lastTime = Duration.ZERO;
+    private static Instant beginTime = Instant.now();
+    private static double deltaTime = fpsDeltatime.toMillis() - lastTime.toMillis();
+
+    public static void calcBeginTime(){
+        beginTime = Instant.now();
+        fpsDeltatime = Duration.ZERO;
+    }
+    public static void calcDeltaTime(){
+        fpsDeltatime = Duration.between(beginTime, Instant.now());
+        deltaTime = (double) fpsDeltatime.toMillis() - lastTime.toMillis();
+        lastTime = fpsDeltatime;
+    }
+    public static double getDeltaTime() {
+        return deltaTime / 1000;
+    }
+}
